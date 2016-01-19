ad07b0f4425747a197e86b568dc5866a2a1b15df
Steven <steventhon94@gmail.com>
2016-01-18 19:16:20 -0800

    Add 05HS to hold until Magento fix

--- parser.py
+++ parser.py
@@ -10,7 +10,7 @@ import fileinput
 # List of specific product SKUs to set to hold
 skusToHold = ["04HS90", "19PS01"]
 # List of substrings of specific product SKUs to set to hold
-subskusToHold = ["01HS", "01PP", "02CW", "02DD", "02SS", "04AM", "04RIG1", "04SF", "06HS", "12BW", "19HSIFC", "fg", "_rescue"]
+subskusToHold = ["01HS", "01PP", "02CW", "02DD", "02SS", "04AM", "04RIG1", "04SF", "05HS", "06HS", "12BW", "19HSIFC", "fg", "_rescue"]
 # List of shipping methods to hold if shipped to POBOX
 poboxShippingsToHold = ["1GD", "FES", "FE2"]
 # List of shipping methods to hold to cover drop-shipped products
