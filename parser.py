79b22a1909a89acf53ba9e886287816fe40fc8b7
Steven <steventhon94@gmail.com>
2016-01-31 14:25:56 -0800

    Refactoring with product list

--- parser.py
+++ parser.py
@@ -10,7 +10,7 @@ import fileinput
 # List of specific product SKUs to set to hold
 skusToHold = ["04HS90", "19PS01"]
 # List of substrings of specific product SKUs to set to hold
-subskusToHold = ["01HS", "01RI97", "02CW", "02DD", "02SS", "04AM", "04RIG1", "04SF", "05HS", "06HS", "12BW", "19HSIFC", "fg", "_rescue"]
+subskusToHold = ["01HS", "01RI97", "02CW", "02DD", "02SS", "04AM", "04RIG1", "04SF", "05HS", "06HS", "12BW", "19HSIFC", "fg"]
 # List of shipping methods to hold if shipped to POBOX
 poboxShippingsToHold = ["1GD", "FES", "FE2"]
 # List of shipping methods to check to cover drop-shipped products
@@ -46,103 +46,6 @@ def removeExceptions(products):
       if subsku in sku:
         products.remove(sku)
   return products
-
-# checkHold returns True if any hold condition is met
-def checkHold(root):
-  hold = False
-  log = ''
-  
-  # Return True if Dick Dixon orders
-  if root.find('lastname').text == 'Dixon' and root.find('firstname').text == 'Dick':
-    log += 'Order placed by ' + root.find('firstname').text + ' ' + root.find('lastname').text + '. Add Signature Required\n'
-    hold = True
-
-  # Add products to a list for easy checking
-  products = []
-  products.append(root.find('product01').text)
-  # Return True if two or more products are ordered
-  if root.find('product02').text is not None:
-    log += 'Found two or more products being ordered\n'
-    hold = True
-    products.append(root.find('product02').text)
-    if root.find('product03').text is not None:
-      products.append(root.find('product03').text)
-      if root.find('product04').text is not None:
-        products.append(root.find('product04').text)
-        if root.find('product05').text is not None:
-          products.append(root.find('product05').text)
-
-  list = [sku for sku in skusToHold if sku in products]
-  # Return True if any of the products' skus is in skus list
-  if list:
-    #print 'Found an element of sku list in products list'
-    log += 'Found Product SKUs: ' + ', '.join(list) + '\n'
-    hold = True
-
-  list = [sku for subsku in subskusToHold for sku in products if subsku in sku]
-  # Return True if any of the products' substrings is in subskus list
-  if list:
-    #print 'Found an element of subsku list as a substring of an element in products list'
-    log += 'Found Product Partial SKUs: ' + ', '.join(list) + '\n'
-    hold = True
-
-  # Return True if product contains SKU '01PPC' and ('X', 'Y', or 'Z')
-  list = [product for product in products if "01PPC" in product and any(custom in product for custom in ('X','Y','Z'))]
-  if list:
-	log += 'Custom Thermo Panel(s). Make sure correct height selected: ' + ', '.join(list) + '\n'
-	hold = True
-
-  shipvia = root.find('shipvia').text
-  
-  # Return True if no shipping method
-  if shipvia is None:
-    log += 'Shipvia: No Shipping Method\n'
-    hold = True
-  # Return True if order will be picked up/will call in
-  if shipvia == 'WC':
-    log += 'Shipvia: Will Call/Pick Up order\n'
-    hold = True
-  # Return True if order is being shipped internationally
-  if root.find('scountry').text != 'US':
-    log += 'Scountry: International\n'
-    hold = True
-  # Return True if order is being shipped via UPS
-  if shipvia == 'United Parcel Service - UPS Ground' or shipvia == 'UG':
-    log += 'Shipvia: Changed ' + shipvia + ' to UG\n'
-    root.find('shipvia').text = 'UG'
-    hold = True
-  # Return True if order is being shipped to a possible P.O. Box via FedEx (1 Day Ground, Standard Overnight, 2 Day Air)
-  if ((root.find('saddress1').text is not None and 'p' == root.find('saddress1').text[0].lower() or
-       root.find('saddress2').text is not None and 'p' == root.find('saddress2').text[0].lower()) and
-       shipvia in poboxShippingsToHold):
-    log += 'Saddress1/Saddress2 and Shipvia: Possible P.O. Box ship and ' + shipvia + '\n'
-    hold = True
-  
-  # Return True if promocode is a set discount (ends with "!")
-  if (root.find('promo_code').text is not None and root.find('promo_code').text[-1:] == '!'):
-    log += "promo_code '" + root.find('promo_code').text + "' is a set amount (ends with '!')\nCoupon code in MOM\n"
-    hold = True
-  
-  # Returns False if no condition is met
-  return (log, hold)
-
-# Add years to date d
-def add_years(d, years):
-    """Return a date that's `years` years after the date (or datetime)
-    object `d`. Return the same calendar date (month and day) in the
-    destination year, if it exists, otherwise use the following day
-    (thus changing February 29 to March 1).
-    """
-    try:
-        return d.replace(year = d.year + years)
-    except ValueError:
-        return d + (datetime(d.year + years, 1, 1).date() - datetime(d.year, 1, 1).date())
-
-# Set the hold date to current date plus 'holdYears' years
-def setHolddate(root, holdYears):
-  holddate = add_years(datetime.now().date(), holdYears)
-  root.find('holddate').text = str(holddate)
-  return 'Hold date set to: ' + str(holddate) + '\n'
 
 # Create a subelement for suborder to add virtual rescue product
 def createRescueProduct(root):
@@ -193,13 +96,117 @@ def createRescueProduct(root):
 		log += "_rescue found. Virtual rescue product created\n"
 	return log
 
-# Removes substring from end of string if exists
+# Removes substring from end of string if exists in root
 def removeEnd(root, end):
-	for i in range(1, 6):
-		if (root.find('product0' + str(i)).text is None):
-			break
-		elif (root.find('product0' + str(i)).text[-len(end):] == end):
-			root.find('product0' + str(i)).text = root.find('product0' + str(i)).text[:-len(end)]
+	found = False
+	for subroot in root:
+		for i in range(1, 6):
+			if (subroot.find('product0' + str(i)).text is None):
+				break
+			elif (subroot.find('product0' + str(i)).text[-len(end):] == end):
+				found = True
+				subroot.find('product0' + str(i)).text = subroot.find('product0' + str(i)).text[:-len(end)]
+	return found
+
+# Return a list of products in the order
+def createProductList(root):
+  products = []
+
+  for subroot in root:
+    for i in range(1, 6):
+      if (subroot.find('product0' + str(i)).text is None):
+        break
+      else:
+        products.append(subroot.find('product0' + str(i)).text)
+  return products
+  
+# Check the product list and returns a log if hold condition is met
+def checkProducts(products):
+  log = ''
+  
+  # If two or more products are ordered
+  if len(products) >= 2:
+    log += 'Found two or more products being ordered: ' + ', '.join(products) + '\n'
+  
+  list = [sku for sku in skusToHold if sku in products]
+  # If any of the products' skus is in skus list
+  if list:
+    log += 'Found Product SKUs: ' + ', '.join(list) + '\n'
+
+  list = [sku for subsku in subskusToHold for sku in products if subsku in sku]
+  # If any of the products' substrings is in subskus list
+  if list:
+    log += 'Found Product Partial SKUs: ' + ', '.join(list) + '\n'
+
+  # If product contains SKU '01PPC' and ('X', 'Y', or 'Z')
+  list = [product for product in products if "01PPC" in product and any(custom in product for custom in ('X','Y','Z'))]
+  if list:
+	log += 'Custom Thermo Panel(s). Make sure correct height selected: ' + ', '.join(list) + '\n'
+	
+  return log
+  
+# checkHold returns a log if any hold condition is met
+def checkHold(root):
+  log = ''
+  
+  # Check all the products for hold
+  log += checkProducts(createProductList(root))
+  
+  # If a rescue product was found, add the virtual rescue product and remove "_rescue" from rescue product skus
+  if removeEnd(root, "_rescue"):
+    log += createRescueProduct(root)
+  
+  # Only need to check the first child for name and shipping information
+  subroot = root[0]
+  
+  # If Dick Dixon orders
+  if subroot.find('lastname').text == 'Dixon' and subroot.find('firstname').text == 'Dick':
+    log += 'Order placed by ' + subroot.find('firstname').text + ' ' + subroot.find('lastname').text + '. Add Signature Required\n'
+
+  shipvia = subroot.find('shipvia').text
+  
+  # If no shipping method
+  if shipvia is None:
+    log += 'Shipvia: No Shipping Method\n'
+  # If order will be picked up/will call in
+  if shipvia == 'WC':
+    log += 'Shipvia: Will Call/Pick Up order\n'
+  # If order is being shipped internationally
+  if subroot.find('scountry').text != 'US':
+    log += 'Scountry: International\n'
+  # If order is being shipped via UPS
+  if shipvia == 'United Parcel Service - UPS Ground' or shipvia == 'UG':
+    log += 'Shipvia: Changed ' + shipvia + ' to UG\n'
+    subroot.find('shipvia').text = 'UG'
+  # If order is being shipped to a possible P.O. Box via FedEx (1 Day Ground, Standard Overnight, 2 Day Air)
+  if ((subroot.find('saddress1').text is not None and 'p' == subroot.find('saddress1').text[0].lower() or
+       subroot.find('saddress2').text is not None and 'p' == subroot.find('saddress2').text[0].lower()) and
+       shipvia in poboxShippingsToHold):
+    log += 'Saddress1/Saddress2 and Shipvia: Possible P.O. Box ship and ' + shipvia + '\n'
+  
+  # If promo_code is a set discount (ends with "!")
+  if (subroot.find('promo_code').text is not None and subroot.find('promo_code').text[-1:] == '!'):
+    log += "promo_code '" + subroot.find('promo_code').text + "' is a set amount (ends with '!')\nCoupon code in MOM\n"
+  
+  return log
+
+# Add years to date d
+def add_years(d, years):
+    """Return a date that's `years` years after the date (or datetime)
+    object `d`. Return the same calendar date (month and day) in the
+    destination year, if it exists, otherwise use the following day
+    (thus changing February 29 to March 1).
+    """
+    try:
+        return d.replace(year = d.year + years)
+    except ValueError:
+        return d + (datetime(d.year + years, 1, 1).date() - datetime(d.year, 1, 1).date())
+
+# Set the hold date to current date plus 'holdYears' years
+def setHolddate(root, holdYears):
+  holddate = add_years(datetime.now().date(), holdYears)
+  root.find('holddate').text = str(holddate)
+  return 'Hold date set to: ' + str(holddate) + '\n'
 
 # Main function to run script
 def main():
@@ -216,33 +223,21 @@ def main():
     # For each XML file, parse each
     for file in files:
 		log = ''
-		count = 1
-		onHold = False
+		products = []
 		print 'Looking in .xml file: ' + file
 		root = ET.parse(file).getroot()
-		# For each suborder in the order
-		for subroot in root:
-			# Check the shipping methods and add custom info if needed
-			checkShipping(subroot)
-			# Check and add holddate if conditions are met
-			olog, onHold = checkHold(subroot)
-			if (onHold):
-				log += 'Found hold in products ' + str(count) + '-' + str(count + 4) + '\n' + olog
-			count += 5
-		# If a rescue product was found, add the virtual rescue product
-		if ('_rescue' in log):
-			for subroot in root:
-				removeEnd(subroot, "_rescue")
-			orderlog += createRescueProduct(root)
-		# If order is on hold
-		if onHold:
+		# Check the shipping methods and add custom info if needed
+		checkShipping(root[0])
+		# If order is on hold, set the hold date
+		log = checkHold(root)
+		if log:
 			# Set hold date by X number of years from now
 			orderlog += 'Alternate Order #' + file[11:-4] + '\n' + log + setHolddate(root[0], 4) + '\n'
 			# Write tree back to XML file
 			ET.ElementTree(root).write(file)
-	# If something went on hold
-    if orderlog != '':
-	    # Open file to write order log in
+	# If something went on hold in any of the files
+    if orderlog:
+	    # Create a file to write the order log in
 		f = open('C:\Users\Administrator\Desktop\logsXtento\orders_' + datetime.now().strftime("%y-%m-%d_%H_%M") + '.txt', 'w')
 		f.write(orderlog)
 
